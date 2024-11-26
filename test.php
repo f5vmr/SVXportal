19703d6e49daa35ebbe60745770a0359a14b8ec2
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-11-26 16:54:23 +0000

    update

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Test the proxy connection
-$json_test = file_get_contents($serveraddress, false, $ctx);
-echo "Proxy connection status: ";
-echo "<br>";
-echo "Source address: http://192.168.1.213:8181";
-echo "<br>";
-echo "Data received through proxy: ";
-var_dump($json_test);
