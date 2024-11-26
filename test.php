3bb1f4aa5d01506314205f8ec187cd18327b6c7a
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-11-26 18:27:16 +0000

    update

--- test.php
+++ test.php
@@ -1,6 +1,7 @@
 <?php
 // Test the proxy connection
 $serveraddress = "http://192.168.1.213:8181/status";
+$ctx = stream_context_create();
 $json_test = file_get_contents($serveraddress, false, $ctx);
 echo "Proxy connection status: ";
 echo "<br>";
